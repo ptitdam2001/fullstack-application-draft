b9afa3161d106756e8776d824863ae586d361ff8
Suhard Damien <ptitdam2001@gmail.com>
2022-07-18 09:50:03 +0200

    Install redis cluster on Dockerfile

--- apps/client/src/Calendar/pages/main.tsx
+++ apps/client/src/Calendar/pages/main.tsx
@@ -5,8 +5,8 @@ import { DragDropCalendar } from '../components'
 export const CalendarMainPage = () => {
   const [events, setEvents] = React.useState<Event[]>([])
 
-  const onEventResize = ({ event, start, end, isAllDay }) => {
-    console.log('Event Resize =>', event, start, end, isAllDay)
+  const onEventResize = (/* { event, start, end, isAllDay }*/ evt: any) => {
+    console.log('Event Resize =>', evt )
   }
 
   const handleSelectSlot = (slot: SlotInfo) => {
